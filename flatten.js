fa6f6ab7107f77442df4af765a03e395e64a827f
Charlie <charlieryancurry@gmail.com>
2022-10-04 04:12:46 +0000

    github crashed on vagrant so redoing

---
+++ flatten.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length >= arr2.length) {
+    arrLength = arr1.length
+  } else if (arr2.length > arr1.length) {
+    arrLength = arr2.length
+  }
+  let bool = true
+  for (let i = 0; i < arrLength; i++) {
+    if (arr1[i] === arr2[i]) {
+      bool = true
+    } else {
+      bool = false
+    }
+  } return bool
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  const errmsg = `🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+  const passmsg = `✅ Assertion Passed: ${arr1} === ${arr2}`;
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(passmsg)
+  } else if (eqArrays(arr1, arr2) === false) {
+    console.log(errmsg)
+  }
+}
+
+const flatten = function(array) {
+  let arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let x = 0; x < array[i].length; x++) {
+        arr.push(array[i][x]);
+      }
+    } else {
+      arr.push(array[i]);
+    }
+  } return arr;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([3, 4, 6, [7, 9], 10, 12]), [3, 4, 6, 7, 9, 10, 12]);
