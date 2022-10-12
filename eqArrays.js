0b7ee68935abd00f227ff72786d44e1868f9b5f0
Charlie <charlieryancurry@gmail.com>
2022-10-12 21:35:45 +0000

    refactored with require/export

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const errmsg = `🛑 Assertion Failed: ${actual} !== ${expected}`;
-  const passmsg = `✅ Assertion Passed: ${actual} === ${expected}`;
-  if (actual === expected) {
-    console.log(passmsg);
-  } else {
-    console.log(errmsg);
-  }
-};
-
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length >= arr2.length) {
-//     arrLength = arr1.length;
-//   } else if (arr2.length > arr1.length) {
-//     arrLength = arr2.length;
-//   }
-//   let bool = true;
-//   for (let i = 0; i < arrLength; i++) {
-//     if (arr1[i] === arr2[i]) {
-//       bool = true;
-//     } else {
-//       bool = false;
-//     }
-//   } return bool;
-// };
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -38,21 +12,7 @@ const eqArrays = function(arr1, arr2) {
     } return bool;
   }
 };
-  
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
-assertEqual(eqArrays([],[]), true);
-assertEqual(eqArrays(["Word"],["Word"]), true);
-
+module.exports = eqArrays;
 
 
---
+++ test/eqArraysTest.js
@@ -0,0 +1,17 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
+assertEqual(eqArrays([],[]), true);
+assertEqual(eqArrays(["Word"],["Word"]), true);
