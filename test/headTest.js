bd78fe1a73122768d40711e71c99468050fc93c4
Charlie <charlieryancurry@gmail.com>
2022-10-13 23:34:28 +0000

    made tests with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([9,10,11]), 10);
-assertEqual(head(["Lighthouse", "Hello", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it('returns "Lighthouse" for ["Lighthouse", "Hello", "Labs"]', () => {
+    assert.strictEqual(head(["Lighthouse", "Hello", "Labs"]), 'Lighthouse'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+  it("returns '9' for [9,10,11]", () => {
+    assert.strictEqual(head([9,10,11]), 9); 
+  });
+});
